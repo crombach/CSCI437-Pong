7ad36bd43faea014524fc125be78efd83d5029e8
Cullen Rombach <cullenr@imagemattersllc.com>
2017-09-17 17:00:21 -0400

    Add compiled release for testing purposes

---
+++ Release/CMakeFiles/pong_core.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cullen/Code/school/pong/src/Ball.cpp" "/home/cullen/Code/school/pong/Release/CMakeFiles/pong_core.dir/src/Ball.cpp.o"
+  "/home/cullen/Code/school/pong/src/CollisionUtils.cpp" "/home/cullen/Code/school/pong/Release/CMakeFiles/pong_core.dir/src/CollisionUtils.cpp.o"
+  "/home/cullen/Code/school/pong/src/Paddle.cpp" "/home/cullen/Code/school/pong/Release/CMakeFiles/pong_core.dir/src/Paddle.cpp.o"
+  "/home/cullen/Code/school/pong/src/ScoreLabel.cpp" "/home/cullen/Code/school/pong/Release/CMakeFiles/pong_core.dir/src/ScoreLabel.cpp.o"
+  "/home/cullen/Code/school/pong/src/TextUtils.cpp" "/home/cullen/Code/school/pong/Release/CMakeFiles/pong_core.dir/src/TextUtils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Release/CMakeFiles/pong_core.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pong_core.dir/src/Ball.cpp.o"
+  "CMakeFiles/pong_core.dir/src/CollisionUtils.cpp.o"
+  "CMakeFiles/pong_core.dir/src/Paddle.cpp.o"
+  "CMakeFiles/pong_core.dir/src/ScoreLabel.cpp.o"
+  "CMakeFiles/pong_core.dir/src/TextUtils.cpp.o"
+  "libpong_core.pdb"
+  "libpong_core.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pong_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
